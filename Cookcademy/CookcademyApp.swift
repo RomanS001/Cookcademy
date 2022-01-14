9a1643384cf3106fd1bb93173bc79489e08d0c9c
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-01-15 00:31:40 +0300

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Roman on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
