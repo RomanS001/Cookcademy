425933f2fd4f7baa982a57c60dcaeffa2d3b8446
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-01-16 02:51:43 +0300

    Finished: build the recipe category grid view.

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 class RecipeData: ObservableObject {
-    
-    @Published var recipes = Recipe.testRecipes
-    
+  @Published var recipe = Recipe.testRecipes
+ 
+  func recipes(for category: MainInformation.Category) -> [Recipe] {
+    var filteredRecipes = [Recipe]()
+    for recipe in recipe {
+      if recipe.mainInformation.category == category {
+        filteredRecipes.append(recipe)
+      }
+    }
+    return filteredRecipes
+  }
+  
 }
